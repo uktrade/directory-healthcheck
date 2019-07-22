c31ec6d3f11d9263d56d6b03da58d4493061c325
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-07-22 13:16:05 +0100

    reduce stnnp warning threshold

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_healthcheck',
-    version='1.2.3',
+    version='1.1.3',
     url='https://github.com/uktrade/directory-healthcheck',
     license='MIT',
     author='Department for International Trade',
