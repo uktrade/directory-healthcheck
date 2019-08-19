86d858dec4bfc8f8d9564a8975c07afc6645f26a
Will Taylor <wrftaylor@gmail.com>
2019-08-19 16:15:44 +0100

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_healthcheck',
-    version='1.1.2',
+    version='1.2.0',
     url='https://github.com/uktrade/directory-healthcheck',
     license='MIT',
     author='Department for International Trade',
